e21ad9dde60342205cac858473baa4ff785ce54c
Prince Philips <phil@Princes-MacBook-Pro.local>
2026-08-09 02:57:50 +0000

    Exclude icon.svg from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -19,6 +19,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)",
+    "/((?!login|api/auth|_next/static|_next/image|favicon.ico|icon.svg).*)",
   ],
 };
