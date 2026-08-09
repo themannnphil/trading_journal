eb1a3dd1e3a4fb528123f770df526edd32c23402
Prince Philips <phil@Princes-MacBook-Pro.local>
2026-08-09 00:15:10 +0000

    Skip ESLint during build for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
